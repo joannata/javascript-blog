fb9a3a4c9100484e625720821489855d468f20e3
Joanna Tarnowska <joanna.tarnowska.91@gmail.com>
2022-03-15 15:23:36 +0100

    Generate title links in script.js file

--- js/script.js
+++ js/script.js
@@ -37,12 +37,6 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
@@ -53,18 +47,43 @@ function generateTitleLinks(){
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
+  console.log('titleList', titleList);
 
   /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log('articles', articles);
+  console.log('optArticleSelector', optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
 
     /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('articleId', articleId);
 
     /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-    /* get the title from the title element */
+    /* [DONE IN PREVIOUS LINE] get the title from the title element */
 
     /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML', linkHTML);
 
     /* insert link into titleList */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+  console.log('html', html);
+
+  const links = document.querySelectorAll('.titles a');
+  console.log('links', links);
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 
 }
 
